4a8676deb72d97b0d58d4b76d60e089a597da5a9
oh <oh@brainslab.kr>
2024-02-20 13:23:05 +0900

    Models Commit

---
+++ WpfDemo/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfDemo.Models;
+
+namespace WpfDemo.ViewModels
+{
+    public class MainWindowViewModel
+    {
+
+
+        public MainWindowViewModel() { }
+    }
+}
